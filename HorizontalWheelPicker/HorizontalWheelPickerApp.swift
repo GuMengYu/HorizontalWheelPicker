68826861c197a80302791403b23151e64abc2442
hyu <yuhong01@agree.com.cn>
2025-11-09 08:47:38 +0800

    Initial Commit

---
+++ HorizontalWheelPicker/HorizontalWheelPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalWheelPickerApp.swift
+//  HorizontalWheelPicker
+//
+//  Created by Yoda on 2025/11/9.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalWheelPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
